d6671c572348e455622fbba00c30e28dbb823cd5
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-09 09:34:57 +0300

    test read data from disk

--- boot.lua
+++ boot.lua
@@ -1,6 +1,12 @@
 local gpu    = component.proxy(
     component.list("gpu")()
 )
+local eeprom = component.proxy(
+    component.list("eeprom")()
+)
+local disk   = component.proxy(
+    eeprom.getData()
+)
 
 local screen_addr = component.list("screen")()
 
@@ -11,6 +17,17 @@ local width, height = gpu.getResolution()
 -- Clear screen
 gpu.fill(1, 1, width, height, " ")
 
+local text
+if disk.exists("text.txt") then
+    fd=disk.open("text.txt")
+    text=disk.read(fd, math.huge)
+    disk.close(fd)
+else
+    text="Hello, world text"
+end
+
 while 1 do
+    --gpu.fill(1, 1, width, height, " ")
     gpu.set(1, 1, "Hello, world!")
+    gpu.set(1, 2, text)
 end
