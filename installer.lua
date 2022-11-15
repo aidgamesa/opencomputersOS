4b619b7023ff0638d413930f49f5cbb59f416ac5
aidgamesa <ya.ivanov1302@yandex.ru>
2022-11-15 15:10:37 +0300

    installer done

--- installer.lua
+++ installer.lua
@@ -22,8 +22,9 @@ if network_component.isHttpEnabled()~=true then
 end
 fs_components=component.list("filesystem")
 for address,_ in fs_components do
-	if component.proxy(address).getLabel()~="tmpfs" then
+	if component.proxy(address).getLabel()~="tmpfs" and component.proxy(address).isReadOnly()~=true then
 		fs_component=component.proxy(address)
+		break
 	end
 end
 print("Installing...")
@@ -40,7 +41,28 @@ local getData = function(url)
 		end
 		os.sleep(0.05)
 	end
-	data=req.read(math.huge)
+	data=""
+	while 1 do
+		chunk=req.read(math.huge)
+		if chunk then
+			data=data..chunk
+		else
+			break
+		end
+	end
 	return data
 end
 installer_data=load(getData("installer_data.lua"))()
+for i = 1,#installer_data do
+	print("Installing '"..installer_data[i].."'...")
+	dir=fileDirectory(installer_data[i])
+	if fs_component.exists(dir) ~= true then
+		fs_component.makeDirectory(dir)
+	end
+	fd=fs_component.open(installer_data[i], "w")
+	fs_component.write(fd, getData(installer_data[i]))
+	fs_component.close(fd)
+end
+print("Flashing EEPROM...")
+eeprom=component.proxy(component.list("eeprom")())
+eeprom.set(getData("bios/bios.lua"))
